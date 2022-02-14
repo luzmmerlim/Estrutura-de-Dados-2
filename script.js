7ec07427b56614b94b50d268781618aaae2e3bde
luzmmerlim <luziamerlim.1309@gmail.com>
2022-02-14 16:48:48 -0300

    exercicio de estrutura de dados

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Luzia";
+let número = 20;
+let anoAtual = 2022 - número;
+
+console.log (anoAtual)
+console.log(número + anoAtual)
+console.log ("Estou Mexendo No console.log")
+
+let quartaFeira = true;
+console.log(quartaFeira)
+console.log (typeof quartaFeira
